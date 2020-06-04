fc9edf1eae29fbf89d5d2edd64bde5ff508e21de
jaminyah <jaminyah@gmail.com>
2020-06-04 03:58:48 -0500

    Add client and server captcha code.

--- client/src/Components/Captcha/Captcha.js
+++ client/src/Components/Captcha/Captcha.js
@@ -1,10 +1,105 @@
 import React, { Component } from 'react';
-
+//import axios from'axios';
+/*
 class Captcha extends Component {
 
     render() {
         return (
             <div>Captcha Image placeholder.</div>
+        );
+    }
+}
+
+export default Captcha;
+*/
+
+
+class Captcha extends Component {
+
+    constructor(props) {
+        super(props);
+        /*this.state = {
+            captchaImage: 'pending ...'
+        }*/
+        this.state = {
+            form: {
+                ShowLineOptions: [],
+                CaptchaType: "math",
+                Id: '',
+                VerifyValue: '',
+                DriverAudio: {
+                    Length: 6,
+                    Language: 'en'
+                },
+                DriverString: {
+                    Height: 60,
+                    Width: 240,
+                    ShowLineOptions: 0,
+                    NoiseCount: 0,
+                    Length: 6,
+                    Fonts: ["wqy-microhei.ttc"],
+                    BgColor: {R: 0, G: 0, B: 0, A: 0},
+                },
+                DriverMath: {
+                    Height: 34,
+                    Width: 240,
+                    ShowLineOptions: 0,
+                    NoiseCount: 0,
+                    Length: 6,
+                    Fonts: ["wqy-microhei.ttc"],
+                    BgColor: {R: 0, G: 0, B: 0, A: 0},
+                },
+                DriverDigit: {
+                    Height: 80,
+                    Width: 240,
+                    Length: 5,
+                    MaxSkew: 0.7,
+                    DotCount: 80
+                },
+                blob: "",
+                loading: false
+            }
+        }
+    }
+
+ 
+
+   assignCaptcha(captcha) {
+        this.setState({
+            form: {blob: captcha.data}
+        });
+    }
+
+    componentDidMount() {
+        console.log('Generating captcha');
+        var fetchData = {
+            method: 'post',
+            body: JSON.stringify(this.state.form),
+            headers: new Headers()
+        }
+ 
+        fetch('/api/getCaptcha', fetchData)
+        .then(function(response){
+            console.log(" fetch .then");
+            return response.json();
+        })
+        .then(
+            (result) => {
+                this.setState({
+                    form: {blob: result.data}
+                })
+            }
+        )
+        .catch(function(error){
+            console.log("fetch error: ")
+            console.log(error);
+        });
+
+    }
+
+    render() {
+       return (
+            <img src={this.state.form.blob} alt='captcha' />
         );
     }
 }
--- server/main.go
+++ server/main.go
@@ -1,101 +1,110 @@
-/* Main driver function. 
+/* Main driver function.
+ * Run: go run main.go fetch.go submit.go
+ */
+/* Main driver function.
  * Run: go run main.go fetch.go submit.go
  */
 
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"database/sql"
-	"encoding/json"
 
-	_"github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/mojocn/base64Captcha"
-	"github.com/GeertJohan/go.rice"
 )
 
 var database, db_err = sql.Open("sqlite3", "./wxalert.db")
-
+var store = base64Captcha.DefaultMemStore
 
 type UserComment struct {
-	Id int				`json:"id"`
-	Name string			`json:"name"`
-	Message string		`json:"comment"`
-	Timestamp string	`json:"date"`
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	Message   string `json:"comment"`
+	Timestamp string `json:"date"`
 }
 
 //configJsonBody json request body.
 type configJsonBody struct {
-	Id              string
-	CaptchaType     string
-	VerifyValue     string
-	ConfigAudio     base64Captcha.ConfigAudio
-	ConfigCharacter base64Captcha.ConfigCharacter
-	ConfigDigit     base64Captcha.ConfigDigit
+	Id            string
+	CaptchaType   string
+	VerifyValue   string
+	DriverAudio   *base64Captcha.DriverAudio
+	DriverString  *base64Captcha.DriverString
+	DriverChinese *base64Captcha.DriverChinese
+	DriverMath    *base64Captcha.DriverMath
+	DriverDigit   *base64Captcha.DriverDigit
 }
 
 // base64Captcha create http handler
 func generateCaptchaHandler(w http.ResponseWriter, r *http.Request) {
-
 	//parse request parameters
 	decoder := json.NewDecoder(r.Body)
-	var postParameters configJsonBody
-	err := decoder.Decode(&postParameters)
+	var param configJsonBody
+	err := decoder.Decode(&param)
 	if err != nil {
 		log.Println(err)
 	}
 	defer r.Body.Close()
+	var driver base64Captcha.Driver
 
 	//create base64 encoding captcha
-	var config interface{}
-	switch postParameters.CaptchaType {
+	switch param.CaptchaType {
 	case "audio":
-		config = postParameters.ConfigAudio
-	case "character":
-		config = postParameters.ConfigCharacter
+		driver = param.DriverAudio
+	case "string":
+		driver = param.DriverString.ConvertFonts()
+	case "math":
+		driver = param.DriverMath.ConvertFonts()
+	case "chinese":
+		driver = param.DriverChinese.ConvertFonts()
 	default:
-		config = postParameters.ConfigDigit
+		driver = param.DriverDigit
 	}
-	captchaId, captcaInterfaceInstance := base64Captcha.GenerateCaptcha(postParameters.Id, config)
-	base64blob := base64Captcha.CaptchaWriteToBase64Encoding(captcaInterfaceInstance)
-
-	//or you can just write the captcha content to the httpResponseWriter.
-	//before you put the captchaId into the response COOKIE.
-	//captcaInterfaceInstance.WriteTo(w)
-
-	//set json response
+	c := base64Captcha.NewCaptcha(driver, store)
+	id, b64s, err := c.Generate()
+	body := map[string]interface{}{"code": 1, "data": b64s, "captchaId": id, "msg": "success"}
+	if err != nil {
+		body = map[string]interface{}{"code": 0, "msg": err.Error()}
+	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	body := map[string]interface{}{"code": 1, "data": base64blob, "captchaId": captchaId, "msg": "success"}
 	json.NewEncoder(w).Encode(body)
+
+	/*
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		body := map[string]interface{}{"message": "pong pong"}
+		json.NewEncoder(w).Encode(body)
+	*/
 }
 
 // base64Captcha verify http handler
-func captchaVerifyHandle(w http.ResponseWriter, r *http.Request) {
+/*func captchaVerifyHandle(w http.ResponseWriter, r *http.Request) {
 
-	//parse request parameters
-	decoder := json.NewDecoder(r.Body)
-	var postParameters configJsonBody
-	err := decoder.Decode(&postParameters)
-	if err != nil {
-		log.Println(err)
-	}
-	defer r.Body.Close()
-	//verify the captcha
-	verifyResult := base64Captcha.VerifyCaptcha(postParameters.Id, postParameters.VerifyValue)
+	 //parse request parameters
+	 decoder := json.NewDecoder(r.Body)
+	 var postParameters configJsonBody
+	 err := decoder.Decode(&postParameters)
+	 if err != nil {
+		 log.Println(err)
+	 }
+	 defer r.Body.Close()
+	 //verify the captcha
+	 verifyResult := base64Captcha.VerifyCaptcha(postParameters.Id, postParameters.VerifyValue)
 
-	//set json response
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	body := map[string]interface{}{"code": "error", "data": "validation failed", "msg": "captcha failed"}
-	if verifyResult {
-		body = map[string]interface{}{"code": "success", "data": "validation verified", "msg": "captcha verified"}
-	}
-	json.NewEncoder(w).Encode(body)
-}
+	 //set json response
+	 w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	 body := map[string]interface{}{"code": "error", "data": "validation failed", "msg": "captcha failed"}
+	 if verifyResult {
+		 body = map[string]interface{}{"code": "success", "data": "validation verified", "msg": "captcha verified"}
+	 }
+	 json.NewEncoder(w).Encode(body)
+ } */
 
 func main() {
-
 
 	if db_err != nil {
 		log.Fatal(db_err)
@@ -103,12 +112,12 @@ func main() {
 	defer database.Close()
 
 	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY, username TEXT, comment TEXT, date TEXT)")
-	statement.Exec()	
+	statement.Exec()
 
 	//http.Handle("/", http.FileServer(http.Dir("static")))
-	http.Handle("/", http.FileServer(rice.MustFindBox("static").HTTPBox()))
+	//http.Handle("/", http.FileServer(rice.MustFindBox("static").HTTPBox()))
 
-    http.HandleFunc("/submit", submitAjax)
+	http.HandleFunc("/submit", submitAjax)
 
 	http.HandleFunc("/comments", fetchComments)
 
@@ -116,10 +125,10 @@ func main() {
 	http.HandleFunc("/api/getCaptcha", generateCaptchaHandler)
 
 	//api for verify captcha
-	http.HandleFunc("/api/verifyCaptcha", captchaVerifyHandle)
+	//http.HandleFunc("/api/verifyCaptcha", captchaVerifyHandle)
 
 	fmt.Printf("Starting HTTP server...\n")
-    if server_err := http.ListenAndServe(":8088", nil); server_err != nil {
-        log.Fatal(server_err)
-    }
+	if serverErr := http.ListenAndServe(":8088", nil); serverErr != nil {
+		log.Fatal(serverErr)
+	}
 }
