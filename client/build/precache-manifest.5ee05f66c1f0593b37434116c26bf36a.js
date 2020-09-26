6675e3b856ed7f124c81573d3e16e9c4dfa90d2d
jaminyah <jaminyah@gmail.com>
2020-09-25 23:47:36 -0400

    Add go-rice static assets bundler.

---
+++ client/build/precache-manifest.5ee05f66c1f0593b37434116c26bf36a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7b5dc331e671101e2bb4765801f87a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe1b18e21770a14f6e86",
+    "url": "/static/css/2.9c13a856.chunk.css"
+  },
+  {
+    "revision": "b77837680eb3e013fdbf",
+    "url": "/static/css/main.5b5e5ce3.chunk.css"
+  },
+  {
+    "revision": "fe1b18e21770a14f6e86",
+    "url": "/static/js/2.d004bfda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d004bfda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77837680eb3e013fdbf",
+    "url": "/static/js/main.fcf053c6.chunk.js"
+  },
+  {
+    "revision": "090c0d1c26953dda4144",
+    "url": "/static/js/runtime-main.75a6f083.js"
+  },
+  {
+    "revision": "cbccfcde7cbf1719358343205a4ac191",
+    "url": "/static/media/badge.cbccfcde.jpg"
+  },
+  {
+    "revision": "b17266ebb20e2f5cda891528b74ecdd7",
+    "url": "/static/media/dia_advis_blizz.b17266eb.png"
+  },
+  {
+    "revision": "a348ab4598fafabecc05ed916f33e110",
+    "url": "/static/media/dia_advis_flood.a348ab45.png"
+  },
+  {
+    "revision": "a5e02ef471a30176957782dcda2ca26f",
+    "url": "/static/media/dia_advis_ssurge.a5e02ef4.png"
+  },
+  {
+    "revision": "6e882da066150d3821aa7ae14c477c45",
+    "url": "/static/media/dia_warn_icestorm.6e882da0.png"
+  },
+  {
+    "revision": "4f2de1425cf9d77bc31b5991d0418b98",
+    "url": "/static/media/dia_warn_ripcurrent.4f2de142.png"
+  },
+  {
+    "revision": "14e62eaf7727b5166ff443a3055b018d",
+    "url": "/static/media/dia_warn_tropicalstorm.14e62eaf.png"
+  },
+  {
+    "revision": "39ae6e689ade95f74d31f8c83ab0dc83",
+    "url": "/static/media/dia_watch_fldlakeshore.39ae6e68.png"
+  },
+  {
+    "revision": "1f5f36b3c57b3ba06fd5cb43a62ad744",
+    "url": "/static/media/dia_watch_freeze.1f5f36b3.png"
+  },
+  {
+    "revision": "e07b96081a2e904b379343bcb1e5d337",
+    "url": "/static/media/dia_watch_windchill.e07b9608.png"
+  },
+  {
+    "revision": "f65a95902a596f8284ab9ff66538b26e",
+    "url": "/static/media/wxalertschedule.f65a9590.png"
+  },
+  {
+    "revision": "25877fd9c5801bacf8e41640adcb278e",
+    "url": "/static/media/wxdiamondtouch.25877fd9.png"
+  },
+  {
+    "revision": "fdf47e2155dc44ef9251effe7c96d40d",
+    "url": "/static/media/wxerror-999.fdf47e21.png"
+  },
+  {
+    "revision": "2cc599ef373c30f86946e21bc4e7319d",
+    "url": "/static/media/wxforecast.2cc599ef.png"
+  },
+  {
+    "revision": "eeaeb448b11a12046e34de10d4286453",
+    "url": "/static/media/wxmapcallout.eeaeb448.png"
+  },
+  {
+    "revision": "0acbe95208cddd3bf9613d7a205023e3",
+    "url": "/static/media/wxsearch.0acbe952.png"
+  },
+  {
+    "revision": "346e5763afeef2d2fce5aa51b5044d01",
+    "url": "/static/media/wxstation-picker.346e5763.png"
+  },
+  {
+    "revision": "159965047e91848e038a28de9e1b5643",
+    "url": "/static/media/wxtemp-err.15996504.png"
+  },
+  {
+    "revision": "166f7ea35891141e6f1980a3046d9ab1",
+    "url": "/static/media/wxtemp-green.166f7ea3.png"
+  },
+  {
+    "revision": "073059a866e79117c74364dac1c77c77",
+    "url": "/static/media/wxtoolbar.073059a8.png"
+  }
+]);
